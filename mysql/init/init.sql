df94b64a11425d30eeb036297f6a3c83afecb8ce
nbhdx <nbhd404@gmail.com>
2021-04-30 14:47:43 +0900

    fix initialize db

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -2,39 +2,6 @@ SET SESSION FOREIGN_KEY_CHECKS = 0;
 
 BEGIN;
 
-CREATE TABLE company
-(
-    company_cd int NOT NULL,
-    rr_cd int NOT NULL,
-    company_name varchar(255) NOT NULL,
-    company_name_k varchar(255),
-    company_name_h varchar(255),
-    company_name_r varchar(255),
-    company_url varchar(255),
-    company_type int,
-    e_status int,
-    e_sort int,
-    PRIMARY KEY (company_cd)
-);
-
-CREATE TABLE line
-(
-    line_cd int NOT NULL ,
-    company_cd int NOT NULL,
-    line_name varchar(255) NOT NULL,
-    line_name_k varchar(255),
-    line_name_h varchar(255),
-    line_color_c varchar(255),
-    line_color_t varchar(255),
-    line_type int,
-    lon float,
-    lat float,
-    zoom int,
-    e_status int,
-    e_sort int,
-    PRIMARY KEY (line_cd)
-);
-
 CREATE TABLE station
 (
     station_cd int NOT NULL,
@@ -55,17 +22,6 @@ CREATE TABLE station
     PRIMARY KEY (station_cd)
 );
 
-CREATE TABLE station_join
-(
-    line_cd int NOT NULL,
-    station_cd1 int NOT NULL,
-    station_cd2 int NOT NULL,
-    PRIMARY KEY (line_cd, station_cd1, station_cd2)
-);
-
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/company20200619.csv' INTO TABLE company FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/join20210312.csv' INTO TABLE station_join FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/line20210312free.csv' INTO TABLE line FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
 LOAD DATA INFILE '/docker-entrypoint-initdb.d/station20210312free.csv' INTO TABLE station FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
 COMMIT;
